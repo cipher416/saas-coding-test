2eaf3305db8f2cdfccb579b63268710e52f62302
Cristoper Anderson <cristoper.anderson@gmail.com>
2024-09-26 15:43:54 +0700

    chore: change prisma client to singleton

--- app/api/price-history/[productId]/[customerId]/route.ts
+++ app/api/price-history/[productId]/[customerId]/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from "@/utils/db";
 
 export async function GET(
   request: Request,
