3020935d3e7749cbb427337d0fb7abba7095f009
Cristoper Anderson <cristoper.anderson@gmail.com>
2024-09-26 16:25:41 +0700

    feat: change submit input to button

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,29 @@
 import {useState} from "react";
 import PriceHistoryChart from "@/components/PriceHistoryChart";
 
+type PriceHistoryInputType = {
+    customerId: number,
+    productId: number
+}
+
 export default function Home() {
     const [customerId, setCustomerId] = useState<number>(0);
     const [productId, setProductId] = useState<number>(0);
+    const [priceHistoryinput, setPriceHistoryInput] = useState<PriceHistoryInputType>({productId: 1, customerId:1})
   return (
-    <div className="">
-        <div>
+    <div className="flex flex-col max-h-screen items-center justify-center min-h-screen min-w-screen">
+        <div className='flex-row space-x-5'>
             <input type={"number"} placeholder={"Enter CustomerID"}
                    onChange={(event) => setCustomerId(Number(event.target.value))}/>
             <input type={"number"} placeholder={"Enter ProductID"}
                    onChange={(event) => setProductId(Number(event.target.value))}/>
+            <button onClick={() => setPriceHistoryInput({customerId, productId})}>
+                Get Data
+            </button>
         </div>
-
-        <PriceHistoryChart customerId={customerId} productId={productId}/>
+        <div className="size-2/4">
+            <PriceHistoryChart customerId={priceHistoryinput.customerId} productId={priceHistoryinput.productId}/>
+        </div>
     </div>
   );
 }
